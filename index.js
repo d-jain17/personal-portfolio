3c803e6ad7597aa84a92aa3379f16e8c94bb1810
d-jain17 <j.deepali2004@gmail.com>
2024-02-07 00:17:05 +0530

    about paragraph added along with parallax effect

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ window.onscroll = function () {
 
 function scrollRotate() {
     let image = document.getElementById("reload");
-    let rotAngle=window.scrollY/11
+    let rotAngle=window.scrollY/2
     image.style.transform = "rotateY(" + rotAngle + "deg)";
+    if(rotAngle>=180){
+        image.style.transform = "rotateY(180deg)"; 
+        window.removeEventListener('scroll', scrollRotate);
+    }
+    else{
     if(rotAngle>90){
         image.style.filter = "grayscale(100%)";
     }
@@ -13,3 +18,16 @@ function scrollRotate() {
         image.style.filter = "none";
     }
 }
+}
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var triggerPosition = 200;
+    var aboutMainDiv = document.querySelector('.about_main_div');
+
+    if (scrollPosition > triggerPosition) {
+        aboutMainDiv.style.right = '10%'; 
+    } else {
+        aboutMainDiv.style.right = '-30%'; 
+    }
+});
+
